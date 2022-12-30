ee0101fc2298ee6aedfef723fa267bc71101dc86
Rordan1 <you@example.com>
2022-12-30 13:35:15 -0500

    fixed version

--- folder1/if-elif-conditional.sh
+++ folder1/if-elif-conditional.sh
@@ -7,7 +7,6 @@ import re
 import sys
 
 
-
 if __name__ == '__main__':
     n = int(input().strip())
     
@@ -21,21 +20,4 @@ if n % 2 != 1 and 6 <= n <= 20:
     print ("Weird")
     
 elif n % 2 != 1 and 20 < n:
-    print ("Not Weird")#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
-
-if __name__ == '__main__':
-    n = int(input().strip())
-    
-if n % 2 == 1:
-    print ("Weird")
-    
-elif 2 <= n <= 5:
     print ("Not Weird")
