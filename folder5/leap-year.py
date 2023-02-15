6d6133473cbf6b40659f767fbe9962f4a259bc25
Rordan1 <87041881+Rordan1@users.noreply.github.com>
2023-02-15 18:16:45 -0500

    updated syntax

--- folder5/leap-year.py
+++ folder5/leap-year.py
@@ -2,7 +2,7 @@ def is_leap(year):
     # Write your logic here
     if (year % 4 == 0) and (year % 100 != 0):
         leap = True
-    if (year % 400 == 0) and (year % 100 == 0):
+    elif (year % 400 == 0) and (year % 100 == 0):
         leap = True
     else:
         leap = False
